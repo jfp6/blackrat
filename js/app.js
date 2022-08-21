2b11ad7255a60f91163bbbce4d087a69617d0b5b
Joseph Prince <joseph.f.prince@gmail.com>
2022-08-21 07:56:33 -0600

    first test js separated

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const tableBody = document.getElementById('table-body')
+
+let flights = [
+    {
+        time:"8:11:",
+        destination:"oman",
+        flight:"DL3567",
+        gate:"A21",
+        remarks:"on time"
+    }
+]
